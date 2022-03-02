4f66059baac71913370d2cde218d70b6b59beaf0
RushawnCampbell <shoutme.sean@gmail.com>
2022-03-02 09:56:48 -0500

    created logout route

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
-from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,5 +15,5 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 # Instantiate Flask-Migrate library here
-
+from flask_migrate import Migrate
 from app import views
